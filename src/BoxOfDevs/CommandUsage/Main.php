e4fe2b918e31c8acc3eaefe473ac2d11b0428dcd
jasonwynn10 <jasonwynn10@gmail.com>
2017-05-15 18:31:59 -0400

    corrected other formatting issues
    OCD kills

--- src/BoxOfDevs/CommandUsage/Main.php
+++ src/BoxOfDevs/CommandUsage/Main.php
@@ -29,154 +29,150 @@ class Main extends PluginBase implements Listener {
 
 	public function onEnable(){
 		$this->cmds = new \stdClass();
-        $this->saveDefaultConfig();
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
-        // Loading all commands.
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new RegisterTask($this), 20 * 5); // Registers after all commands are loaded
-    }
-
-    /**
-     * Sets the client command usage
-     * @param Command $cmd
-     * @param string $usage
-     */
-    public function setClientUsage(Command $cmd, string $usage){
-        $cmdData =  json_decode(file_get_contents($this->getServer()->getFilePath()."src/pocketmine/resources/command_default.json"));
-        //Getting the usage.
-        if(substr($cmd->getUsage(), 0,1) == "%"){
-            $usage = $this->getServer()->getLanguage()->translateString(substr($cmd->getUsage(), 1), []);
-        }else{
-            $usage = $cmd->getUsage();
-        }
-        // Parsing arguments
-        preg_match_all("/((<(.+?)>)|(\[(.+?)\]))/", $usage, $matches);
-        $data = [];
-        foreach($matches[0] as $key => $match){
-            $data[] = $this->string2Std($match);
-            if(count($matches[0]) - 1 == $key && isset($data[$key]->type) && $data[$key]->type == "string") $data[$key]->type = "rawtext";
-        }
-        // Setting the command data
-        foreach($cmd->getAliases() as $alias){
-            $this->aliases[$alias] = $cmd->getName();
-        }
-        $cmdData->aliases = $cmd->getAliases();
-        $cmdData->description = $cmd->getDescription();
-        $cmdData->permission = $cmd->getPermission();
-        $cmdData->overloads->default->input->parameters = $data;
-        $this->cmds->{$cmd->getName()} = new \stdClass();
-        $this->cmds->{$cmd->getName()}->versions = [];
-        $this->cmds->{$cmd->getName()}->versions[0] = $cmdData;
-    }
-
-    /**
-     * @param string $data
-     * @return \stdClass|null
-     */
-    protected function string2Std(string $data){
+		$this->saveDefaultConfig();
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		// Loading all commands.
+		$this->getServer()->getScheduler()->scheduleRepeatingTask(new RegisterTask($this), 20 * 5); // Registers after all commands are loaded
+	}
+	/**
+	 * Sets the client command usage
+	 * @param Command $cmd
+	 * @param string $usage
+	 */
+	public function setClientUsage(Command $cmd, string $usage){
+		$cmdData =  json_decode(file_get_contents($this->getServer()->getFilePath()."src/pocketmine/resources/command_default.json"));
+		//Getting the usage.
+		if(substr($cmd->getUsage(), 0,1) == "%"){
+			$usage = $this->getServer()->getLanguage()->translateString(substr($cmd->getUsage(), 1), []);
+		}else{
+			$usage = $cmd->getUsage();
+		}
+		// Parsing arguments
+		preg_match_all("/((<(.+?)>)|(\[(.+?)\]))/", $usage, $matches);
+		$data = [];
+		foreach($matches[0] as $key => $match){
+		$data[] = $this->string2Std($match);
+			if(count($matches[0]) - 1 == $key && isset($data[$key]->type) && $data[$key]->type == "string") $data[$key]->type = "rawtext";
+		}
+		// Setting the command data
+		foreach($cmd->getAliases() as $alias){
+			$this->aliases[$alias] = $cmd->getName();
+		}
+		$cmdData->aliases = $cmd->getAliases();
+		$cmdData->description = $cmd->getDescription();
+		$cmdData->permission = $cmd->getPermission();
+		$cmdData->overloads->default->input->parameters = $data;
+		$this->cmds->{$cmd->getName()} = new \stdClass();
+		$this->cmds->{$cmd->getName()}->versions = [];
+		$this->cmds->{$cmd->getName()}->versions[0] = $cmdData;
+	}
+	/**
+	 * @param string $data
+	 * @return \stdClass|null
+	 */
+	protected function string2Std(string $data){
 		$return = new \stdClass();
-        if(preg_match("/^<(.+?)>$/", $data, $m) > 0){
-            $return->optional = false;
-        }elseif(preg_match("/^\[(.+?)\]$/", $data, $m) > 0){
-            $return->optional = true;
-        }else{
-            $return->name = $data;
-            $return->type = "rawtext";
-            $return->optional = true;
-            return $return;
-        }
-        $return->name = $m[1];
-        switch(true) {
-            case strpos(strtolower($m[0]), "player") || strpos(strtolower($m[0]), "target"):
-            $return->type = "target";
-            break;
-            case ($this->lastCheck == "y" && strpos(strtolower($m[0]), "z")) || strpos(strtolower($m[0]), "x y z") || strpos(strtolower($m[0]), "coords") || strpos(strtolower($m[0]), "pos"):
-            $return->type = "x y z";
-            break;
-            case $this->lastCheck == "x" && strpos(strtolower($m[0]), "y"):
-            $this->lastCheck = "y";
-            return null;
-            break;
-            case strpos(strtolower($m[0]), "x"):
-            $this->lastCheck = "x";
-            return null;
-            break;
-            case strpos(strtolower($m[0]), "number") || strpos(strtolower($m[0]), "int") || strpos(strtolower($m[0]), "id"):
-            $return->type = "int";
-            break;
-            case strpos(strtolower($m[0]), "block") || strpos(strtolower($m[0]), "tile"):
-            $return->type = "block";
-            break;
-            case strpos(strtolower($m[0]), "entity"):
-            $return->type = "entity";
-            break;
-            case strpos(strtolower($m[0]), "gamemode"):
-            $return->type = "gamemode";
-            break;
-            case strpos(strtolower($m[0]), "..."):
-            $return->type = "rawtext";
-            break;
-            default:
-            $return->type = "string";
-            break;
-        }
-        return $return;
-    }
-
-    /**
-     * Checks when a command packet is sent
-     * @priority MONITOR
-     *
-     * @param DataPacketSendEvent $event
-     */
-    public function onDataPacketSend(DataPacketSendEvent $event){
-        if($event->getPacket() instanceof AvailableCommandsPacket){
-            $event->getPacket()->commands = json_encode($this->cmds);
-        }
-    }
-
-    /**
-     * Properly set args back to their original position
-     * @priority MONITOR
-     *
-     * @param DataPacketReceiveEvent $event
-     */
-    public function onDataPacketReceive(DataPacketReceiveEvent $event){
-        if($event->getPacket() instanceof CommandStepPacket){
-            if($event->getPacket()->args !== null){
-                $ordered = "";
-                $args = $event->getPacket()->args;
-                if(!isset($this->cmds->{$event->getPacket()->command})){
-                    $cmd = $this->cmds->{$this->aliases[$event->getPacket()->command]};
-                }else{
-                    $cmd = $this->cmds->{$event->getPacket()->command};
-                }
-                $args2 = $cmd->versions[0]->overloads->{$event->getPacket()->overload}->input->parameters;
-                foreach($args2 as $key => $arg){
-                    if(isset($args->{$arg->name})){
-                        var_dump($args->{$arg->name}); //TODO remove debug
-                        if($args->{$arg->name} instanceof \stdClass){ 
-                            if(isset($args->{$arg->name}->rules)){
-                                $args->{$arg->name} = $args->{$arg->name}->rules[0]->value;
-                            }else{
-                                switch($args->{$arg->name}->selector){
-                                    case "randomPlayer":
-                                    $args->{$arg->name} = "@r";
-                                    break;
-                                    case "nearestPlayer":
-                                    $args->{$arg->name} = "@p";
-                                    break;
-                                    case "allPlayers":
-                                    $args->{$arg->name} = "@a";
-                                    break;
-                                }
-                            }
-                        }
-                        $ordered .= $args->{$arg->name} . " ";
-                    }
-                }
-                $event->getPacket()->args = ["args" => substr($ordered, 0, strlen($ordered) - 1)];
-            }
-        }
-    }
+		if(preg_match("/^<(.+?)>$/", $data, $m) > 0){
+			$return->optional = false;
+		}elseif(preg_match("/^\[(.+?)\]$/", $data, $m) > 0){
+			$return->optional = true;
+		}else{
+			$return->name = $data;
+			$return->type = "rawtext";
+			$return->optional = true;
+			return $return;
+		}
+		$return->name = $m[1];
+		switch(true) {
+			case strpos(strtolower($m[0]), "player") || strpos(strtolower($m[0]), "target"):
+				$return->type = "target";
+			break;
+			case ($this->lastCheck == "y" && strpos(strtolower($m[0]), "z")) || strpos(strtolower($m[0]), "x y z") || strpos(strtolower($m[0]), "coords") || strpos(strtolower($m[0]), "pos"):
+				$return->type = "x y z";
+			break;
+			case $this->lastCheck == "x" && strpos(strtolower($m[0]), "y"):
+				$this->lastCheck = "y";
+				return null;
+			break;
+			case strpos(strtolower($m[0]), "x"):
+				$this->lastCheck = "x";
+				return null;
+			break;
+			case strpos(strtolower($m[0]), "number") || strpos(strtolower($m[0]), "int") || strpos(strtolower($m[0]), "id"):
+				$return->type = "int";
+			break;
+			case strpos(strtolower($m[0]), "block") || strpos(strtolower($m[0]), "tile"):
+				$return->type = "block";
+			break;
+			case strpos(strtolower($m[0]), "entity"):
+				$return->type = "entity";
+			break;
+			case strpos(strtolower($m[0]), "gamemode"):
+				$return->type = "gamemode";
+			break;
+			case strpos(strtolower($m[0]), "..."):
+				$return->type = "rawtext";
+				break;
+			default:
+				$return->type = "string";
+		}
+		return $return;
+	}
+	/**
+	 * Checks when a command packet is sent
+	 * @priority MONITOR
+	 *
+	 * @param DataPacketSendEvent $event
+	 */
+	public function onDataPacketSend(DataPacketSendEvent $event){
+		if($event->getPacket() instanceof AvailableCommandsPacket){
+			$event->getPacket()->commands = json_encode($this->cmds);
+		}
+	}
+	/**
+	 * Properly set args back to their original position
+	 * @priority MONITOR
+	 *
+	 * @param DataPacketReceiveEvent $event
+	 */
+	public function onDataPacketReceive(DataPacketReceiveEvent $event){
+		if($event->getPacket() instanceof CommandStepPacket){
+			if($event->getPacket()->args !== null){
+				$ordered = "";
+				$args = $event->getPacket()->args;
+				if(!isset($this->cmds->{$event->getPacket()->command})){
+					$cmd = $this->cmds->{$this->aliases[$event->getPacket()->command]};
+				}else{
+					$cmd = $this->cmds->{$event->getPacket()->command};
+				}
+				$args2 = $cmd->versions[0]->overloads->{$event->getPacket()->overload}->input->parameters;
+				foreach($args2 as $key => $arg){
+					if(isset($args->{$arg->name})){
+						if(!$this->isPhar()) {
+							var_dump($args->{$arg->name});
+						}
+						if($args->{$arg->name} instanceof \stdClass){
+							if(isset($args->{$arg->name}->rules)){
+								$args->{$arg->name} = $args->{$arg->name}->rules[0]->value;
+							}else{
+								switch($args->{$arg->name}->selector){
+									case "randomPlayer":
+										$args->{$arg->name} = "@r";
+									break;
+									case "nearestPlayer":
+										$args->{$arg->name} = "@p";
+									break;
+									case "allPlayers":
+										$args->{$arg->name} = "@a";
+									break;
+								}
+							}
+						}
+						$ordered .= $args->{$arg->name} . " ";
+					}
+				}
+				$event->getPacket()->args = ["args" => substr($ordered, 0, strlen($ordered) - 1)];
+			}
+		}
+	}
 }
